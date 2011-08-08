147b4d548a0404808636fd84710863dec970098a
kdayton <kdayton@KEVINDNB7E.Acceleration.biz>
2011-08-08 13:32:41 +0200

    Tat ta test

--- BoomiScripts/src/DataProcessScripts/Hello.java
+++ BoomiScripts/src/DataProcessScripts/Hello.java
@@ -2,8 +2,7 @@ package DataProcessScripts;
 
 public class Hello {
 	public static void main(String[] args) {
-		// Here we are adding a commit just because
-		System.out.println("This is a pointless piece of code");
+		
 	}
 
 }
