f836eadd41eb440fff3d7337ace0a0f1788c00e6
kdayton <kdayton@KEVINDNB7E.Acceleration.biz>
2011-08-08 11:03:28 +0200

    User update test

--- BoomiScripts/src/DataProcessScripts/Hello.java
+++ BoomiScripts/src/DataProcessScripts/Hello.java
@@ -3,7 +3,7 @@ package DataProcessScripts;
 public class Hello {
 	public static void main(String[] args) {
 		// Here we are adding a commit just because
-		System.out.println("This is a pointless poece of code");
+		System.out.println("This is a pointless piece of code");
 	}
 
 }
